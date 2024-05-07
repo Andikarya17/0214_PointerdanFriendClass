6c3876dbe7996495ba58aa1462869c892b248dcd
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-07 15:08:47 +0700

    membuat display luas persegi dan luas segitiga

--- 0214_PointerdanFriendClass.cpp
+++ 0214_PointerdanFriendClass.cpp
@@ -3,29 +3,44 @@ using namespace std;
  
 class persegiPanjang 
 {
-
-
-    public : 
-        int panjang, lebar;
+public : 
+    int panjang, lebar;
         int luasPersegiPanjang(int b, int a)
         {
             return a * b;
+        }
+
+        int hitung(persegiPanjang p)
+        {
+            return p.panjang;
         }
 };
 
 class segitiga
 {
-    public : 
-        int tinggi,
-            alas;
+public : 
+    int tinggi,
+        alas;
 
-        int luasSegitiga(int a, int t)
+    int luasSegitiga(int a, int t)
         {
             return 0.5 * a * t;
         }
 };
 
 int main(){
-    
+    persegiPanjang pp;
+    segitiga sg;
+    cout << "Masukan Panjang : ";
+    cin >> pp.panjang;
+    cout << "Masukan lebar : ";
+    cin >> pp.lebar;
+    cout << "Masukan Tinggi : ";
+    cin >> sg.tinggi;
+    cout << "Masukan Alas : ";
+    cin >> sg.alas;
+    cout << "Luas Persegi Panjang : " << pp.luasPersegiPanjang(pp.panjang, pp.lebar) <<endl;
+    cout << "Luas Segitiga : " << sg.luasSegitiga(sg.tinggi, sg.alas) <<endl;
+
 
 }
